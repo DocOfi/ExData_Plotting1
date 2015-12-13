e50c873cd8f55c996c8495735783849b69a91310
DocOfi <eofilada@yahoo.com>
2015-12-13 14:24:45 +0800

    Updating my assignment for signature track

--- plot1.R
+++ plot1.R
@@ -1,3 +1,9 @@
+## Downloading the data
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "household_power_consumption.zip")
+dateDownloaded <- date()
+print(dateDownloaded)
+## [1] "Sun Dec 13 13:02:11 2015"
 ## Reading the data in R
 elec <- read.csv("household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";", na.strings = "?")
 ## Converting the Date Variable from Character to Date
--- plot2.R
+++ plot2.R
@@ -1,7 +1,13 @@
+## Downloading the data
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "household_power_consumption.zip")
+dateDownloaded <- date()
+print(dateDownloaded)
+## [1] "Sun Dec 13 13:02:11 2015"
 ## Reading the data in R
 elec <- read.csv("household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";", na.strings = "?")
 ## Converting the Date Variable from Character to Date
-elec$Date <- as.Date(elec$Date, format = "%d/%m/%Y")
+elec$Date <- strptime(elec$Date, format = "%d/%m/%Y")
 ## Subsetting the particular data we need
 feb1and2 <- elec[elec$Date >= "2007-02-01" & elec$Date <= "2007-02-02", ]
 ## Creating a new variable
